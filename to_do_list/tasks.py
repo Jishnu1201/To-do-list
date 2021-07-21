461b1b6d020d95684bda5181fb42c188b9e94437
Jishnu K <jishnuk.1201@gmail.com>
2021-07-21 11:11:23 +0530

    Able to add tasks, updated tasks.py and addtask.html

--- to_do_list/tasks.py
+++ to_do_list/tasks.py
@@ -22,10 +22,36 @@ def dashboard():
     oby = request.args.get("order_by", "id")  
     order = request.args.get("order", "asc")
     if order == "asc":
-        cursor.execute(f"select l.id, l.task, l.created, l.due, l.status from list l order by l.{oby}")
+        cursor.execute(f"select id, task, created, due, status from list order by {oby}")
     else:
-        cursor.execute(f"select l.id, l.task, l.created, l.due, l.status from list l order by l.{oby} desc")
+        cursor.execute(f"select id, task, created, due, status from list order by {oby} desc")
     tasks = cursor.fetchall()
     return render_template('index.html', tasks = tasks, order="desc" if order=="asc" else "asc")
 
 
+@bp.route("/add", methods=["GET", "POST"])
+def add():
+    conn = db.get_db()
+    cursor = conn.cursor()
+    if request.method == "GET":
+        data = dict(id = "",
+                    task = "",
+                    created = "",
+                    due = "",
+                    description = "",
+                    status = "")
+        return render_template("addtask.html", **data)
+    elif request.method == "POST":
+        task = request.form.get("task")
+        created = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+        due = request.form.get("due")
+        due = due.replace("T", "-")
+        due = due.split("-")
+        due = due[2] + "/" +  due[1] + "/" +  due[0] + " " + due[3]
+        description = request.form.get('description')
+        status = "Not completed"
+        cursor.execute("INSERT INTO list (task, created, due, description, status) values (?, ?, ?, ?, ?)", (task, created, due, description, status))
+        conn.commit()
+      
+        return redirect(url_for("tasks.dashboard"), 302)
+        
